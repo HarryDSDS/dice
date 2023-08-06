f9fc9d412f093b5bd5b0afa37d42045071e8937b
HarryDSDS <samelakandroid@gmail.com>
2023-08-06 21:15:50 +0200

    yes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // selectors czy chuj
 const diceShowcase = document.querySelector(".dice-showcase");
 const diceGrid = document.querySelector(".dice-grid");
+const diceGridSelected = document.querySelector('.dice-grid-selected')
 
 // buttons
 const btnRollAll = document.querySelector(".btn-roll-all");
@@ -32,10 +33,107 @@ const diceArray = [
     name: "6",
     img: "img/6.png",
   },
+  {
+    name: "1",
+    img: "img/1.png",
+  },
+  {
+    name: "2",
+    img: "img/2.png",
+  },
+  {
+    name: "3",
+    img: "img/3.png",
+  },
+  {
+    name: "4",
+    img: "img/4.png",
+  },
+  {
+    name: "5",
+    img: "img/5.png",
+  },
+  {
+    name: "6",
+    img: "img/6.png",
+  },
+  {
+    name: "1",
+    img: "img/1.png",
+  },
+  {
+    name: "2",
+    img: "img/2.png",
+  },
+  {
+    name: "3",
+    img: "img/3.png",
+  },
+  {
+    name: "4",
+    img: "img/4.png",
+  },
+  {
+    name: "5",
+    img: "img/5.png",
+  },
+  {
+    name: "6",
+    img: "img/6.png",
+  },
+  {
+    name: "1",
+    img: "img/1.png",
+  },
+  {
+    name: "2",
+    img: "img/2.png",
+  },
+  {
+    name: "3",
+    img: "img/3.png",
+  },
+  {
+    name: "4",
+    img: "img/4.png",
+  },
+  {
+    name: "5",
+    img: "img/5.png",
+  },
+  {
+    name: "6",
+    img: "img/6.png",
+  },
+  {
+    name: "1",
+    img: "img/1.png",
+  },
+  {
+    name: "2",
+    img: "img/2.png",
+  },
+  {
+    name: "3",
+    img: "img/3.png",
+  },
+  {
+    name: "4",
+    img: "img/4.png",
+  },
+  {
+    name: "5",
+    img: "img/5.png",
+  },
+  {
+    name: "6",
+    img: "img/6.png",
+  },
 ];
 
 let diceChosen = [];
 let diceChosenID = [];
+
 // functions
 
 function randomDiceNumber(min, max) {
@@ -43,42 +141,40 @@ function randomDiceNumber(min, max) {
 }
 
 function rollAll() {
-  // sortowanie, za kazdym odpaleniem rollAll shufflujemy tablicę
   diceArray.sort(() => 0.5 - Math.random());
-
-  // oramy w pizdu tego diceGrida, czyli wszystkie kości, przez ułamek sekundy jest pusto na ekranie
   while (diceGrid.firstChild) {
     diceGrid.removeChild(diceGrid.lastChild);
   }
 
-  // zamiast 6 na sztywno, pętla idzie do diceArray.length. Raz, ze wiadomo po czym iterujemy, dwa, jak zmieni się (akurat nie w tym przypadku)
-  // ilość elementów w tablicy, to pętla to będzie respektować
-  for (let i = 0; i < diceArray.length; i++) {
-    // tworzymy nowy element HTML img, jeszcze go nie rysujemy, on jest tylko w pamięci
+  for (let i = 0; i < 5; i++) {
     const dice = document.createElement("img");
-
-    // z posortowanej listy bierzemy sobie name czyli 1,2,3 whatever
     const diceName = diceArray[i].name;
-
-    // uzywamy tego name, zeby dokleic do .png (to w pizdu mozna uprościć, next time)
     dice.setAttribute("src", `img/${diceName}.png`);
-    dice.setAttribute("id", `${diceName}`);
-
-    // doklejamy, dzieki temu jest widoczne w UI, a jako, ze pętla idzie po kadej kości, to będzie 6 kości
+    dice.setAttribute("id", i);
     diceGrid.appendChild(dice);
     console.log(diceGrid);
-
-    // stylujemy to gówno
     dice.style.marginRight = "4px";
     dice.style.borderRadius = "10px";
     diceGrid.style.height = "120px";
     diceGrid.style.display = "block";
   }
 }
-diceGrid.addEventListener("click", () => {
-  console.log(diceGrid[2]);
-});
+
+function diceSelect(){
+diceGrid.addEventListener("click", (e) => {
+  const clickedDice = e.target
+  diceChosen.push(clickedDice)
+  console.log(diceChosen)
+  // console.log(diceChosen)
+//   for (i = 0; i < diceChosen.length; i++){
+
+// }
+}
+)}
+
 
 const diceButtonRollAll = btnRollAll.addEventListener("click", () => {
+  console.log(diceGrid.innerHTML)
   rollAll();
+  diceSelect()
 });
