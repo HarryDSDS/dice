5f8ea00320a02a036076e5830548d5a481f6eb95
HarryDSDS <dominiksamelak@gmail.com>
2023-08-15 16:50:15 +0200

    kurwaaaaa

--- app.js
+++ app.js
@@ -130,16 +130,14 @@ const diceArray = [
   },
 ];
 
+let diceRolledGrid = [];
 let diceChosen = [];
 let diceChosenID = [];
 let rollCount = 0;
 
 // functions
-// do zrobienia:
-// - deselekcja w diceGrid i jednoczesne usunięcie z diceChosen
 
 function rollAll() {
-  rollCount++;
   diceGridSelected.innerHTML = "";
   diceChosen = [];
   diceArray.sort(() => 0.5 - Math.random());
@@ -154,6 +152,7 @@ function rollAll() {
     dice.setAttribute("class", `${diceName}`);
     dice.setAttribute("id", `${i}`);
     diceGrid.appendChild(dice);
+    diceRolledGrid.push(dice.name);
     dice.style.marginRight = "4px";
     dice.style.borderRadius = "10px";
     diceGrid.style.height = "120px";
@@ -161,32 +160,10 @@ function rollAll() {
   }
 }
 
-// function diceGridDeselect() {
-//   diceGrid.addEventListener("click", (e) => {
-//     const clickedDice = e.target;
-//     for (let i = 0; i < diceChosen.length; i++) {
-//       const dice = document.createElement("img");
-//       const diceName = clickedDice.name;
-//       console.log(diceName);
-
-//       dice.setAttribute("src", `img/${diceName}.png`);
-//       dice.setAttribute("name", `${diceName}`);
-//       dice.setAttribute("class", `${diceName}`);
-//       dice.setAttribute("id", `${i}`);
-//       diceChosen.pull(clickedDice);
-//       dice.style.marginRight = "4px";
-//       dice.style.borderRadius = "10px";
-//       diceGrid.style.height = "120px";
-//       diceGrid.style.display = "block";
-//     }
-//   });
-// }
-// diceGridDeselect();
-
 function rollSelected() {
   diceGridSelected.innerHTML = "";
   btnRollSelected.addEventListener("click", () => {
-    //
+    rollCount++;
     while (diceGridSelected.firstChild) {
       diceGridSelected.removeChild(diceGridSelected.lastChild);
     }
@@ -196,6 +173,7 @@ function rollSelected() {
       const diceName = diceArray[i].name;
       dice.setAttribute("src", `img/${diceName}.png`);
       dice.setAttribute("name", `${diceName}`);
+      dice.setAttribute("index", `${i}`);
       diceGridSelected.appendChild(dice);
       dice.style.marginRight = "4px";
       dice.style.borderRadius = "10px";
@@ -206,18 +184,23 @@ function rollSelected() {
 }
 
 function diceSelect() {
+  const rollValue = diceRolledGrid.values();
   diceGrid.addEventListener("click", (e) => {
     const clickedDice = e.target;
+
     btnRollSelected.addEventListener("click", () => {
       diceGrid.removeChild(clickedDice);
+      for (const value of rollValue) {
+        if (clickedDice.id == value) {
+        }
+      }
+      //
     });
     const nameDice = clickedDice.name;
     diceChosen.push(clickedDice);
     for (i = 0; i < diceChosen.length; i++) {
       clickedDice.setAttribute("src", `img/${nameDice}rev.png`);
-      clickedDice.setAttribute("id", `${i}`);
     }
-    console.log(diceChosen);
   });
 }
 
@@ -233,16 +216,44 @@ function diceDeselect() {
       dice.style.borderRadius = "10px";
       diceGridSelected.removeChild(clickedDice);
       diceGrid.appendChild(dice);
+      diceRolledGrid.push(dice.name);
       diceChosen.pop();
-      // debugger;
     }
   });
+}
+// sprawdza czy dana kość została wyrzucona
+function diceCheck() {
+  const count = {};
+  const rollValue = diceRolledGrid.values();
+  for (let ele of rollValue) {
+    if (count[ele]) {
+      count[ele] += 1;
+    } else {
+      count[ele] = 1;
+    }
+  }
+}
+
+function getOccurrence(array, value) {
+  let count = 0;
+  array.forEach((v) => v === value && count++);
+}
+
+function rollCountCount() {
+  if (rollCount === 4) {
+    alert("next round");
+    rollCount = 1;
+  }
 }
 
 rollSelected();
 diceSelect();
 btnRollAll.addEventListener("click", () => {
+  rollCount++;
+  diceRolledGrid.splice(0, 5);
   diceChosen = [];
   rollAll();
   diceDeselect();
+  rollCountCount();
+  diceCheck();
 });
