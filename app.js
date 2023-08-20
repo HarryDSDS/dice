86820b8bbf682da61e6b5767dd7a875803cd6bdf
HarryDSDS <dominiksamelak@gmail.com>
2023-08-20 22:31:20 +0200

    logika dla szkółki ok

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // selectors czy chuj
 const diceGrid = document.querySelector(".dice-grid");
 const diceGridSelected = document.querySelector(".dice-grid-selected");
-const oneDiceDiv = document.querySelector(".one-dice");
+const schoolOne = document.querySelector(".school-one");
+const schoolTwo = document.querySelector(".school-two");
+const schoolThree = document.querySelector(".school-three");
+const schoolFour = document.querySelector(".school-four");
+const schoolFive = document.querySelector(".school-five");
+const schoolSix = document.querySelector(".school-six");
 
 // buttons
 const btnRollAll = document.querySelector(".btn-roll-all");
 const btnRollSelected = document.querySelector(".btn-roll-selected");
+const btnNextRound = document.querySelector(".next-round");
+const btnSchoolConfirmOne = document.querySelector(".confirm-one");
+const btnSchoolConfirmTwo = document.querySelector(".confirm-two");
+const btnSchoolConfirmThree = document.querySelector(".confirm-three");
+const btnSchoolConfirmFour = document.querySelector(".confirm-four");
+const btnSchoolConfirmFive = document.querySelector(".confirm-five");
+const btnSchoolConfirmSix = document.querySelector(".confirm-six");
 
 // arrays
 const diceArray = [
@@ -134,12 +146,19 @@ const diceArray = [
 let diceChosen = [];
 let diceChosenID = [];
 let rollCount = 0;
-let oneDice = 0;
+let oneDie = 0;
 let twoDice = 0;
 let threeDice = 0;
 let fourDice = 0;
 let fiveDice = 0;
 let sixDice = 0;
+let oneLogicEnable = true;
+let twoLogicEnable = true;
+let threeLogicEnable = true;
+let fourLogicEnable = true;
+let fiveLogicEnable = true;
+let sixLogicEnable = true;
+let rollSelectedEnable = true; // wyłacza zerowanie wartości kości
 
 // functions
 
@@ -163,7 +182,7 @@ function rollAll() {
     diceGrid.style.height = "120px";
     diceGrid.style.display = "block";
     if (dice.name === "1") {
-      oneDice++;
+      oneDie++;
     }
     if (dice.name === "2") {
       twoDice++;
@@ -203,9 +222,8 @@ function rollSelected() {
       dice.style.borderRadius = "10px";
       diceGridSelected.style.height = "120px";
       diceGridSelected.style.display = "block";
-      console.log(dice);
       if (dice.name === "1") {
-        oneDice++;
+        oneDie++;
       }
       if (dice.name === "2") {
         twoDice++;
@@ -222,7 +240,24 @@ function rollSelected() {
       if (dice.name === "6") {
         sixDice++;
       }
-      console.log(oneDice);
+      if (oneLogicEnable) {
+        oneDieLogic();
+      }
+      if (twoLogicEnable) {
+        twoDiceLogic();
+      }
+      if (threeLogicEnable) {
+        threeDiceLogic();
+      }
+      if (fourLogicEnable) {
+        fourDiceLogic();
+      }
+      if (fiveLogicEnable) {
+        fiveDiceLogic();
+      }
+      if (sixLogicEnable) {
+        sixDiceLogic();
+      }
     }
   });
 }
@@ -230,7 +265,6 @@ function rollSelected() {
 function diceSelect() {
   diceGrid.addEventListener("click", (e) => {
     const clickedDice = e.target;
-
     btnRollSelected.addEventListener("click", () => {
       diceGrid.removeChild(clickedDice);
     });
@@ -245,6 +279,7 @@ function diceSelect() {
 function diceDeselect() {
   diceGridSelected.addEventListener("click", (e) => {
     const clickedDice = e.target;
+    console.log(clickedDice.name);
     for (let i = 0; i < diceChosen.length; i++) {
       const dice = document.createElement("img");
       const diceName = clickedDice.name;
@@ -254,45 +289,229 @@ function diceDeselect() {
       dice.style.borderRadius = "10px";
       diceGridSelected.removeChild(clickedDice);
       diceGrid.appendChild(dice);
-      oneDiceLogic();
+      if (oneLogicEnable) {
+        oneDieLogic();
+      }
+      if (twoLogicEnable) {
+        twoDiceLogic();
+      }
+      if (threeLogicEnable) {
+        threeDiceLogic();
+      }
+      if (fourLogicEnable) {
+        fourDiceLogic();
+      }
+      if (fiveLogicEnable) {
+        fiveDiceLogic();
+      }
+      if (sixLogicEnable) {
+        sixDiceLogic();
+      }
       diceChosen.pop();
     }
   });
 }
 
 function rollCountCount() {
-  if (rollCount === 4) {
-    alert("next round");
-    rollCount = 0;
+  if (rollCount === 3) {
+    btnRollAll.disabled = true;
+    btnRollSelected.disabled = true;
   }
 }
 
 rollSelected();
 diceSelect();
 btnRollAll.addEventListener("click", () => {
-  oneDice = 0;
+  oneDie = 0;
+  twoDice = 0;
+  threeDice = 0;
+  fourDice = 0;
+  fiveDice = 0;
+  sixDice = 0;
   rollCount++;
   diceChosen = [];
   rollAll();
   diceDeselect();
   rollCountCount();
-  oneDiceLogic();
+  if (oneLogicEnable) {
+    oneDieLogic();
+  }
+  if (twoLogicEnable) {
+    twoDiceLogic();
+  }
+  if (threeLogicEnable) {
+    threeDiceLogic();
+  }
+  if (fourLogicEnable) {
+    fourDiceLogic();
+  }
+  if (fiveLogicEnable) {
+    fiveDiceLogic();
+  }
+  if (sixLogicEnable) {
+    sixDiceLogic();
+  }
+});
+
+// function rollSelectedRun() {
+//   btnRollSelected.addEventListener("click", () => {
+//     console.log(rollSelectedEnable);
+
+//     if (rollSelectedEnable) {
+//       rollSelectedEnable = false;
+//       oneDie = 0;
+//       twoDice = 0;
+//       threeDice = 0;
+//       fourDice = 0;
+//       fiveDice = 0;
+//       sixDice = 0;
+//       console.log(rollSelectedEnable);
+//     }
+//   });
+// }
+rollSelectedRun();
+btnSchoolConfirmOne.addEventListener("click", () => {
+  oneLogicEnable = false;
+  btnSchoolConfirmOne.disabled = true;
+  btnRollAll.disabled = true;
+  btnRollSelected.disabled = true;
+});
+btnSchoolConfirmTwo.addEventListener("click", () => {
+  twoLogicEnable = false;
+  btnSchoolConfirmTwo.disabled = true;
+  btnRollAll.disabled = true;
+  btnRollSelected.disabled = true;
+});
+btnSchoolConfirmThree.addEventListener("click", () => {
+  threeLogicEnable = false;
+  btnSchoolConfirmThree.disabled = true;
+  btnRollAll.disabled = true;
+  btnRollSelected.disabled = true;
+});
+btnSchoolConfirmFour.addEventListener("click", () => {
+  fourLogicEnable = false;
+  btnSchoolConfirmFour.disabled = true;
+  btnRollAll.disabled = true;
+  btnRollSelected.disabled = true;
+});
+btnSchoolConfirmFive.addEventListener("click", () => {
+  fiveLogicEnable = false;
+  btnSchoolConfirmFive.disabled = true;
+  btnRollAll.disabled = true;
+  btnRollSelected.disabled = true;
+});
+btnSchoolConfirmSix.addEventListener("click", () => {
+  sixLogicEnable = false;
+  btnSchoolConfirmSix.disabled = true;
+  btnRollAll.disabled = true;
+  btnRollSelected.disabled = true;
 });
 
 //logika
 
 // 1
 
-function oneDiceLogic() {
-  if (oneDice == 1) {
-    oneDiceDiv.innerHTML = -2;
-  } else if (oneDice == 2) {
-    oneDiceDiv.innerHTML = -1;
-  } else if (oneDice == 3) {
-    oneDiceDiv.innerHTML = 0;
-  } else if (oneDice == 4) {
-    oneDiceDiv.innerHTML = 1;
-  } else if (oneDice == 5) {
-    oneDiceDiv.innerHTML = 2;
+function oneDieLogic() {
+  if (oneDie == 1) {
+    schoolOne.innerHTML = -2;
+  } else if (oneDie == 2) {
+    schoolOne.innerHTML = -1;
+  } else if (oneDie == 3) {
+    schoolOne.innerHTML = 0;
+  } else if (oneDie == 4) {
+    schoolOne.innerHTML = 1;
+  } else if (oneDie == 5) {
+    schoolOne.innerHTML = 2;
+  } else {
+    schoolOne.innerHTML = "---";
+  }
+}
+
+// 2
+
+function twoDiceLogic() {
+  if (twoDice == 1) {
+    schoolTwo.innerHTML = -4;
+  } else if (twoDice == 2) {
+    schoolTwo.innerHTML = -2;
+  } else if (twoDice == 3) {
+    schoolTwo.innerHTML = 0;
+  } else if (twoDice == 4) {
+    schoolTwo.innerHTML = 2;
+  } else if (twoDice == 5) {
+    schoolTwo.innerHTML = 4;
+  } else {
+    schoolTwo.innerHTML = "---";
+  }
+}
+
+// 3
+function threeDiceLogic() {
+  if (threeDice == 1) {
+    schoolThree.innerHTML = -6;
+  } else if (threeDice == 2) {
+    schoolThree.innerHTML = -3;
+  } else if (threeDice == 3) {
+    schoolThree.innerHTML = 0;
+  } else if (threeDice == 4) {
+    schoolThree.innerHTML = 3;
+  } else if (threeDice == 5) {
+    schoolThree.innerHTML = 6;
+  } else {
+    schoolThree.innerHTML = "---";
+  }
+}
+
+// 4
+
+function fourDiceLogic() {
+  if (fourDice == 1) {
+    schoolFour.innerHTML = -8;
+  } else if (fourDice == 2) {
+    schoolFour.innerHTML = -4;
+  } else if (fourDice == 3) {
+    schoolFour.innerHTML = 0;
+  } else if (fourDice == 4) {
+    schoolFour.innerHTML = 4;
+  } else if (fourDice == 5) {
+    schoolFour.innerHTML = 8;
+  } else {
+    schoolFour.innerHTML = "---";
+  }
+}
+
+// 5
+
+function fiveDiceLogic() {
+  if (fiveDice == 1) {
+    schoolFive.innerHTML = -10;
+  } else if (fiveDice == 2) {
+    schoolFive.innerHTML = -5;
+  } else if (fiveDice == 3) {
+    schoolFive.innerHTML = 0;
+  } else if (fiveDice == 4) {
+    schoolFive.innerHTML = 5;
+  } else if (fiveDice == 5) {
+    schoolFive.innerHTML = 10;
+  } else {
+    schoolFive.innerHTML = "---";
+  }
+}
+
+// 6
+
+function sixDiceLogic() {
+  if (sixDice == 1) {
+    schoolSix.innerHTML = -12;
+  } else if (sixDice == 2) {
+    schoolSix.innerHTML = -6;
+  } else if (sixDice == 3) {
+    schoolSix.innerHTML = 0;
+  } else if (sixDice == 4) {
+    schoolSix.innerHTML = 6;
+  } else if (sixDice == 5) {
+    schoolSix.innerHTML = 12;
+  } else {
+    schoolSix.innerHTML = "---";
   }
 }
